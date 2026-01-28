5c6ac93272bf9d1eda1ed166ea062ae02e9c5623
mdenitti <massimodn77@gmail.com>
2026-01-28 11:54:36 +0100

    Implement user registration functionality in generate.php

--- generate.php
+++ generate.php
@@ -3,6 +3,17 @@ include 'includes/header.php';
 require 'includes/conn.php';
 $_SESSION['email'] = $_POST['email'];
 $_SESSION['password'] = $_POST['password'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+// the unsafe way; 
+$query="INSERT INTO users (name, email,password) VALUES ('ljlkj','$email','$password')";
+if(mysqli_query($conn,$query)) {
+    echo "New user create successfully";
+} else {
+    echo "Error, try again: ".mysqli_error($conn);
+}
+
 ?>
     <div class="container">
 
