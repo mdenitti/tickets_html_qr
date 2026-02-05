24e4661e6269b41125547ce39146ce2a004bdfe8
mdenitti <massimodn77@gmail.com>
2026-02-05 15:45:35 +0100

    refactor: Update database connection method to use mysqli object-oriented style

--- includes/conn.php
+++ includes/conn.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "qrtickets";
 
-if (!($conn = mysqli_connect($servername, $username, $password, $dbname))) {
-    die("Connection failed: " . mysqli_connect_error());
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
